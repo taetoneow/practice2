22e245aa4644aeecf1d186ae9eba91fdce6ee4dd
Tae Yama <yamasakimclaughlintae@gmail.com>
2019-07-10 10:52:19 -0700

    Incorrect usage of import sys attempt 1

--- .vscode/ex13.py
+++ .vscode/ex13.py
@@ -7,3 +7,8 @@ print("Your second variable is:", second)
 print("Your third variable is:", third)
 
 
+
+import sys
+print("This is the name of the script:", sys.argv)
+print("Number of arguments:", len(sys.argv))
+print("The arguments are:", str(sys.argv))
