ac94c43550d51a266704e59b8c2889c9ce19595a
Tae Yama <yamasakimclaughlintae@gmail.com>
2019-07-11 10:50:18 -0700

    Geeks for Geeks coding tutorials str.format()

---
+++ .vscode/GeeksforGeekspythonstrformat.py
@@ -0,0 +1,12 @@
+#Python3 program to demonstrate
+#the str.format() method
+
+#using format option in a simple string
+print("{}, A computer science portal for geeks."
+   .format("GeeksforGeeks"))
+ #using format option for a 
+ # value stored in a variable
+str = "This article is written in {}"
+print  (str.format("Python"))
+ # formatting a string using a numeric constant
+print("Hello, I am {} years old!".format(18))
